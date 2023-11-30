7809e70ea915877c4b6246eef8885af5b0d1800a
Matheus Silva <ms25022003@gmail.com>
2023-11-30 12:09:42 -0300

    first commit

---
+++ src/components/button/index.tsx
@@ -0,0 +1,53 @@
+import { ButtonHTMLAttributes } from "react"
+import { tv, type VariantProps } from "tailwind-variants"
+
+const button = tv({
+  base: "px-4 py-2",
+  variants: {
+    visual: {
+      contained: "!text-white border-none",
+      outlined: "border-2 !bg-white",
+    },
+    color: {
+      indigo: "bg-indigo-800 text-indigo-800 border-indigo-800",
+      danger: "bg-danger text-danger border-danger",
+      violet: "bg-violet-300 text-violet-300 border-violet-300",
+    },
+    fullWidth: {
+      true: "w-full",
+    },
+    rounded: {
+      true: "rounded-full",
+    },
+  },
+})
+
+type ButtonVariants = VariantProps<typeof button>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & ButtonVariants
+
+const Button = ({
+  children,
+  rounded,
+  color = "indigo",
+  fullWidth,
+  className,
+  visual = "contained",
+  ...otherProps
+}: ButtonProps) => {
+  return (
+    <button
+      {...otherProps}
+      className={button({
+        fullWidth,
+        color,
+        rounded,
+        visual,
+        class: className,
+      })}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
