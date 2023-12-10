e8d5a65de10f37b857d92951d5ba1bd132b2d0d1
Matheus Silva <ms25022003@gmail.com>
2023-12-10 07:49:37 -0300

    fix: removing nested scrolling

--- src/components/task/TaskList.tsx
+++ src/components/task/TaskList.tsx
@@ -7,9 +7,9 @@ interface TaskListProps {
 
 const TaskList = ({ children, title }: TaskListProps) => {
   return (
-    <div className="flex h-fit max-h-full min-h-[243px] w-full max-w-[330px] flex-1 shrink-0 basis-full flex-col overflow-auto rounded-3xl bg-zinc-100 px-2 py-[14px] shadow-card">
+    <div className="h-fit min-h-[243px] w-full max-w-[330px] flex-1 shrink-0 basis-full flex-col rounded-3xl bg-zinc-100 px-2 py-[14px] shadow-card">
       <h2 className="text-lg font-bold text-neutral-500 sm:text-xl">{title}</h2>
-      <div className="flex-1 overflow-auto px-1">{children}</div>
+      <div className="px-1">{children}</div>
     </div>
   )
 }
